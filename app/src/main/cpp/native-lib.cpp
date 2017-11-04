26c647a15f408c3a964db89e70e560ec7988790b
Kyle Effinger <kyle.effinger@gmail.com>
2017-11-03 17:13:35 -0700

    Real Initial Commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_edu_something_ar_1framework_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
